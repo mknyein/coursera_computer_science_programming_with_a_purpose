bccb9ba5ad32c67639d89a532e61055e2da15232
Khine <mknyein00@gmail.com>
2022-02-20 17:19:04 +0900

    htree and ruler java files - added

---
+++ Ruler.java
@@ -0,0 +1,13 @@
+public class Ruler
+{
+  public static String ruler(int n)
+  {
+    if (n == 0) return " ";
+    return ruler(n-1) + n + ruler(n-1);
+  }
+  public static void main(String[] args)
+  {
+    int n = Integer.parseInt(args[0]);
+    StdOut.println(ruler(n));
+  }
+}
